b99560401c9545fbbf02ea64969a73206babdd13
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-02-23 23:06:56 +0100

    added global variables for header activity

---
+++ Template/scripts/globalVars.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const globalVars = {
+    searchInput: document.getElementById("searchInput"),
+    searchBtn: document.getElementById("searchBtn"),
+    favBtn: document.getElementById("favBtn")
+}
