918b415025d574e78466b56f8ee66aeeb721f71c
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-02-29 09:36:09 +0100

    Created a new script file that will handle the showcasing of specific movie details

---
+++ Template/scripts/movieDetails.js
@@ -0,0 +1,9 @@
+'use strict'
+
+window.addEventListener('load', () => {
+    console.log('load');
+    //Förslagsvis anropar ni era funktioner som skall sätta lyssnare, rendera objekt osv. härifrån
+    
+    globalVars.setupHeaderEvents();
+    
+});
